91f9839db6ec8d54d785fdb3033faaf93e5e8b4c
aritra-123-ux <mr.aritramaity07@gmail.com>
2026-06-08 13:13:33 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,5 @@
+    function calculateage(){
+   let birthyear=document.getElementById("birthyear").value;
+   let age=2026-birthyear;
+   document.getElementById("result").innerText="your Age is "+age;
+}
